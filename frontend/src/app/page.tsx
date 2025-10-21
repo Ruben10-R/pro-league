ca1ebbf482d8f7c13fa78ddc705a7441cb5f7ad8
ruben <ruben@302.nl>
2025-10-21 18:43:50 +0200

    Remove `package-lock.json` to stop tracking dependencies.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { StatesTypes } from "@chrono-tracker/shared";
+import { StatesTypes } from "@pro-league/shared";
 
 export default function Home() {
   return (
